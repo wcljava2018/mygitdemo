a839bbdd89db1932de4da4f2bf36056b94ce2e96
wcljava2018 <1245204467@qq.com>
2020-02-25 20:30:40 +0800

    哈哈哈

---
+++ src/test/java/com/wcl/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcl.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
